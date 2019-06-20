f858eaa7ca24df963d697ea3ded9f7723edd868e
Rafael Balest <rbmurussi@gmail.com>
2019-06-19 21:15:41 -0300

    comment

--- src/test/java/org/clearingio/ipm/MsgIpmTest.java
+++ src/test/java/org/clearingio/ipm/MsgIpmTest.java
@@ -14,31 +14,31 @@ import java.io.*;
 
 public class MsgIpmTest {
 
-	private Logger LOGGER = LoggerFactory.getLogger(MsgIpmTest.class);
-
-	static {
-		//System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "DEBUG");
-	}
-
-	@Test
-	public void test() throws IOException, NotFoundMTIException {
-		MsgBuilder<MsgIpm> builder = new MsgBuilder<>(MsgIpm.class, Encode.EBCDIC);
-		FileIO fileIO = new FileIO(Encode.EBCDIC);
-		File file = fileIO.fromFileBlocked(new File("C:\\Users\\uesr\\Desktop\\rjb"));
-		try(RdwDataInputStream in = new RdwDataInputStream(new FileInputStream(file));
-			DataOutputStream out = new DataOutputStream(new FileOutputStream(file.getAbsolutePath() + ".out"))) {
-			while(in.hasNext()) {
-				byte[] array1 = in.next();
-				LOGGER.info(new String(array1, Encode.EBCDIC.getName()));
-				MsgIpm msg = builder.unpack(array1);
-				LOGGER.info(msg.toString());
-				byte[] array2 = builder.pack(msg);
-				out.writeInt(array2.length);
-				out.write(array2);
-				LOGGER.info(new String(array2, Encode.EBCDIC.getName()));
-			}
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+//	private Logger LOGGER = LoggerFactory.getLogger(MsgIpmTest.class);
+//
+//	static {
+//		//System.setProperty("org.slf4j.simpleLogger.defaultLogLevel", "DEBUG");
+//	}
+//
+//	@Test
+//	public void test() throws IOException, NotFoundMTIException {
+//		MsgBuilder<MsgIpm> builder = new MsgBuilder<>(MsgIpm.class, Encode.EBCDIC);
+//		FileIO fileIO = new FileIO(Encode.EBCDIC);
+//		File file = fileIO.fromFileBlocked(new File("C:\\Users\\uesr\\Desktop\\rjb"));
+//		try(RdwDataInputStream in = new RdwDataInputStream(new FileInputStream(file));
+//			DataOutputStream out = new DataOutputStream(new FileOutputStream(file.getAbsolutePath() + ".out"))) {
+//			while(in.hasNext()) {
+//				byte[] array1 = in.next();
+//				LOGGER.info(new String(array1, Encode.EBCDIC.getName()));
+//				MsgIpm msg = builder.unpack(array1);
+//				LOGGER.info(msg.toString());
+//				byte[] array2 = builder.pack(msg);
+//				out.writeInt(array2.length);
+//				out.write(array2);
+//				LOGGER.info(new String(array2, Encode.EBCDIC.getName()));
+//			}
+//		} catch(Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 }
